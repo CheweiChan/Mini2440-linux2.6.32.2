0a96ae9968e4692dc97f77c1a9f914dc7796b09c
Chewei <a24553120@gmail,com>
2019-03-07 00:23:54 +0800

    update

---
+++ 04-Signal_SIGIO/signal_app.c
@@ -0,0 +1,55 @@
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<stdio.h>
+#include<fcntl.h>
+#include<signal.h>
+#include<unistd.h>
+
+int fd,g_running;
+
+void sync_handler(int num)
+{
+    printf("receive sync signal \n");
+}
+
+void stop_handler(int num)
+{
+    close(fd);
+    g_running=0;
+    printf("stop process \n");
+}
+
+int main(void)
+{
+    int oflags;
+    g_running=1;
+    signal(SIGINT,stop_handler);
+
+    fd =open("/dev/syncdev",O_RDWR)
+    if(fd) printf("open ok \n");
+    else printf("open fail \n");
+
+    signal(SIGIO,sync_handler);
+    fcntl(fd,F_SETOWN,getpid());
+    oflags = fcntl(fd,F_GETFL);
+    fcntl(fd,F_SETFL,oflags|FASYNC);
+    while(g_running)
+    {
+        sleep(100);
+    }
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
